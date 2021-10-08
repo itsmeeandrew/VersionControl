7e954b4c3ab7dbc864e4384f877804a616b9fe20
itsmeeandrew <schin.andras01@gmail.com>
2021-10-08 13:37:51 +0200

    Adatok betöltése

--- UserMaintenance/week04/Form1.cs
+++ UserMaintenance/week04/Form1.cs
@@ -13,9 +13,17 @@ namespace week04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            flats = context.Flat.ToList();
         }
     }
 }
