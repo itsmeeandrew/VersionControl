2982046dea05eeaa7935d82b3ebe8d78d49c88e2
itsmeeandrew <schin.andras01@gmail.com>
2021-10-08 13:44:48 +0200

    Excel munkafüzet létrehozása és megnyitása

--- UserMaintenance/week04/Form1.cs
+++ UserMaintenance/week04/Form1.cs
@@ -17,10 +17,41 @@ namespace week04
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlBook;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlBook = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlBook.ActiveSheet;
+
+                // CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlBook.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlBook = null;
+                xlApp = null;
+            }
         }
 
         private void LoadData()
