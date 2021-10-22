611f5f23ef400f5f3e8215384eab5d91868b1632
itsmeeandrew <schin.andras01@gmail.com>
2021-10-22 11:13:35 +0200

    Adatok betöltése

--- UserMaintenance/week05/Form1.cs
+++ UserMaintenance/week05/Form1.cs
@@ -12,9 +12,13 @@ namespace week05
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> ticks;
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = ticks;
         }
     }
 }
