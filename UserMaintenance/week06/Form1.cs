d316c21fdd6d2990363d9bb66914c414127e00a1
itsmeeandrew <schin.andras01@gmail.com>
2021-10-29 10:57:58 +0200

    Webszolgáltatás hívása, árfolyam lista létrehozása

--- UserMaintenance/week06/Form1.cs
+++ UserMaintenance/week06/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
+using week06.MnbServiceReference;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            doSomething();
+            dataGridView1.DataSource = rates;
+        }
+
+        private void doSomething()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
