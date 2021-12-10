8d1570cf3e62cd9b9dbbe1755e2b78e23b161f2c
itsmeeandrew <schin.andras01@gmail.com>
2021-12-10 11:02:01 +0100

    Iterációs lépés és új generáció

--- UserMaintenance/week12/Form1.cs
+++ UserMaintenance/week12/Form1.cs
@@ -15,12 +15,63 @@ namespace week12
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             Controls.Add(ga);
+
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = $"{generation}. generáció";
+            label1.BringToFront();
+
+            var playerList = from x in gc.GetCurrentPlayers()
+                             orderby x.GetFitness() descending
+                             select x;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                {
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                } else
+                {
+                    gc.AddPlayer(b);
+                }
+
+                if (generation % 3 == 0)
+                {
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                } else
+                {
+                    gc.AddPlayer(b.Mutate());
+                }
+            }
+
+            gc.Start();
         }
     }
 }
